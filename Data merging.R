8da15a87376b722c08308f33fa5f8400d19dfd55
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2025-10-23 16:18:44 +0200

    Update Data merging.R

--- Data merging.R
+++ Data merging.R
@@ -3,12 +3,41 @@ library(readxl)
 library(dplyr)
 library(lubridate)
 library(writexl)
+library(data.table)
 
-setwd("~/Desktop/Università/DiANA/Dottorato/Danimarca/AU/Aeris GHG Graes")
+# I removed this as this path only works on your (Lorenzos) computer. 
+# setwd("~/Desktop/Università/DiANA/Dottorato/Danimarca/AU/Aeris GHG Graes")
+
+# General recommendations: 
+# have separate scripts for N2O and CH4 data. 
+
+# clear workspace
+rm(list = ls())
 
 
 aeris <- read_excel("Excel Aeris.xlsx")
-chambers <- read_excel("chamber_data_complete.xlsx")
+da <- as.data.table(aeris)
+
+dc <- fread("chamber_data_complete.csv")
+setnames(dc, as.character(dc[1, ]))
+dc <- dc[-1, ]
+
+
+# splitting dc in two data.tables, one for N2O and one for CH4 
+dc.n2o <- dc[! is.na(start.N2O), ]
+dc.ch4 <- dc[! is.na(start.CH4), ]
+
+# correcting Aeris time
+# N2O aeris is 1 hour behind real time
+da[ , Datatime := Datatime + 1 * 3600]
+
+# making a date.time column for mearing, rounded to nearest minute
+da[, date.time := as.POSIXct(round(Datatime, "mins"))]
+
+
+
+
+
 
 # Create a column Datetime in the new file
 aeris <- aeris %>%
