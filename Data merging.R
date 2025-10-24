7ed739daffe58ca0d6c0ae8cec85738e055aee74
Johanna Pedersen <96528149+JohannaPedersen@users.noreply.github.com>
2025-10-24 15:52:27 +0200

    working on mearing data

--- Data merging.R
+++ Data merging.R
@@ -11,24 +11,20 @@ library(data.table)
 # I removed this as this path only works on your (Lorenzos) computer. 
 # setwd("~/Desktop/Università/DiANA/Dottorato/Danimarca/AU/Aeris GHG Graes")
 
-# General recommendations: 
-# have separate scripts for N2O and CH4 data. 
-
-
-aeris <- read_excel("Excel Aeris.xlsx")
-da <- as.data.table(aeris)
-
+# loading data
+da <- read_excel("Excel Aeris.xlsx")
 dc <- fread("chamber_data_complete.csv")
+
+# cleaning data
 setnames(dc, as.character(dc[1, ]))
 dc <- dc[-1, ]
-
-
-# splitting dc in two data.tables, one for N2O and one for CH4 
-
 dc$start.N2O <- as.numeric(dc$start.N2O)
 dc$end.N2O <- as.numeric(dc$end.N2O)
 
-dc <- dc[! is.na(start.N2O), ]
+# selecting dc data for N2O 
+dc <- dc[! is.na(start.N2O), c('chamber', 'ID', 'begin.time', 'final.time', 'start.N2O', 'end.N2O')]
+
+da <- as.data.table(da)
 
 # Lorenzo, I am unsure if the data there is in the dc is Aeries time or 'real time'? 
 # if it is Aeris time then the next line correting the time should not be done
@@ -40,19 +36,55 @@ dc <- dc[! is.na(start.N2O), ]
 da[ , date.time := Datatime + 1 * 3600]
 
 # making a date.time column for mearing, rounded to nearest minute
-da[, begin.time := as.POSIXct(round(Datatime, "mins"), tzone = 'UTC')]
-da[, final.time := as.POSIXct(round(Datatime, "mins"), tzone = 'UTC')]
+da[, date.time := as.POSIXct(round(Datatime, "mins"), tzone = 'UTC')]
+
 
 # change dc begin time to same format as date.time in da
-dc[, begin.time := as.POSIXct(begin.time, format = "%d-%m-%Y %H:%M", tzone = 'UTC')]
-dc[, final.time := as.POSIXct(final.time, format = "%d-%m-%Y %H:%M", tzone = 'UTC')]
+dc[, begin.time := as.POSIXct(begin.time, format = '%d-%m-%Y %H:%M', tzone = 'UTC')]
+dc[, final.time := as.POSIXct(final.time, format = '%d-%m-%Y %H:%M', tzone = 'UTC')]
 
-dt <- merge(da, dc, by = "begin.time", all.x = TRUE)
-dt <- merge(dt, dc, by = "final.time", all.x = TRUE)
+# adding rows with chamber, id and N2O info for between begin.time and final.time
+# sequence function for one row
+safe_seq <- function(from, to) {
+  # if to < from, return NA (or return reversed/empty based on preference)
+   if (to < from) return(as.POSIXct(NA_real_))
+  seq(from, to, by = '1 min')
+}
+
+# Expand row-by-row. Use rowid to ensure seq() gets scalars.
+dc[, rowid := .I]
+
+dcl <- dc[, {dcthis <- safe_seq(begin.time[1], final.time[1])
+    .(date.time = dcthis)},
+  by = .(rowid, chamber, ID, start.N2O, end.N2O)]
+
+# drop helper rowid
+dcl[, rowid := NULL]
+
+# challenge that we have some measurements that start and stop on the same times.stamp
+# because the resolution is minutes, not seconds.... 
+# the times we have dublicates: 
+dcl[duplicated(date.time), unique(date.time)]
+
+# making a full mearge, mening that when we have the same date.time for start and stop
+# it will be dublicated
+dt <- merge(da, dcl, by = "date.time", all.x = TRUE, allow.cartesian = TRUE)
+
+# dropping Aeris data when not measured on chamber: 
+dt <- dt[!is.na(start.N2O)]
+
+# # trying to make a plot just to see that is looks ok
+# 
+# library(ggplot2)
+# ggplot(dt, aes(start.N2O, end.N2O, color = chamber)) + 
+#   geom_point() + 
+#   facet_wrap(~ chamber)
+
+# something is wrong, data is missing - e.g. chamber 11 there is only the first days of 
+# measurements and not the subsequent ones. 
+test <- dt[dt$chamber == '11', ]
 
 
-
-# 251024, code below not used, code below not done JP
 
 # Create a column Datetime in the new file
 aeris <- aeris %>%
